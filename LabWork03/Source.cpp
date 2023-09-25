a45142b400231bb51e8eeda1f39a5f7d8b7e3051
LenochkaDomina <105883583+LenochkaDomina@users.noreply.github.com>
2023-09-25 13:57:40 +0300

    Add files via upload

---
+++ LabWork03/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "My name is Domina Olena";
+	std::cout << ", 413gr.";
+	std::cout << std::endl;
+
+	int a = 10;
+	float b = 10.5f;
+	char c = 'd';
+	bool d = true;
+
+	std::cout << "int a=" << a << std::endl;
+	std::cout << "float b=" << b << std::endl;
+	std::cout << "char c=" << c << std::endl;
+	std::cout << "bool b=" << b << std::endl;
+	system("pause");
+	return EXIT_SUCCESS;
+}
